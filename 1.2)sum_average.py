94c0947b4a6911dc46406b2e87504df9fde64633
aakash-namala <84466599+aakash-namala@users.noreply.github.com>
2021-05-30 16:25:40 +0530

    Add files via upload

---
+++ 1.2)sum_average.py
@@ -0,0 +1,9 @@
+#program to print sum and average
+
+print('Enter the three numbers')
+num1=int(input('num1: '))
+num2=int(input('num2: '))
+num3=int(input('num3: '))
+total=num1+num2+num3
+average=total/3
+print('Total of the three numbers is:{}\nAverage of the three numbers is: {}'.format(total,average))
